e65ff7462b41f2f072c549349bb8c4f72270c629
Joshua Campos <jdeveloper1203@gmail.com>
2022-08-30 10:28:18 -0400

    commit tree 14c18ceb1174426454d5782c1e7ada8801759221

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+};
+
+function appendCat(name){
+    const newArray = [...cats];
+    newArray.push(name);
+
+    return newArray;
+};
+
+function prependCat(name){
+    const newArray = [...cats];
+    newArray.unshift(name);
+
+    return newArray;
+};
+
+function removeLastCat(){
+    const newArray = [...cats];
+    newArray.pop();
+
+    return newArray;
+};
+
+function removeFirstCat(){
+    return cats.slice(1);
+};
